3d04cc9090be21e24a0af12fcdd9426e0ad25c0b
SCEPTICG <114002154+SCEPTICG@users.noreply.github.com>
2022-11-05 23:48:11 +0100

    cambiado el metodo de instalación de neovim

--- conf.sh
+++ conf.sh
@@ -25,9 +25,9 @@ echo deb [arch=amd64 signed-by=/usr/share/keyrings/vscode.gpg] https://packages.
 
 #INSTALAMOS TODOS LOS PAQUETES
 sudo apt update
-sudo apt install firefox neovim code nala virtualbox git kitty -y
+sudo apt install firefox code nala virtualbox git kitty -y
 wget https://github.com/neovim/neovim/releases/download/v0.8.0/nvim-linux64.deb
-sudo apt install ./nvim-linux64.deb
+sudo dpkg -i nvim-linux64.deb
 rm nvim-linux64.deb
 
 #DESCARGAMOS E INSTALAMOS NERDFONTS
