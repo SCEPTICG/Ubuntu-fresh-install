cc9148441f77daa43fb1c9857300f88d036ea79d
SCEPTICG <scepticgames001@gmail.com>
2022-11-07 17:59:21 +0100

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -8,6 +8,7 @@ sudo mkdir /usr/share/zsh-sudo
 sudo chown scepticg:scepticg /usr/share/zsh-sudo
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
 sudo mv sudo.plugin.zsh /usr/share/zsh-sudo
+wget https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd-musl_0.23.1_amd64.deb
 sudo dpkg -i lsd-musl_0.23.1_amd64.deb
 mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
