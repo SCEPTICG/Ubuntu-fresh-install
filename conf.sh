be5f330c8e6eee4a2974a954485c6b3e9ac15ef2
SCEPTICG <scepticgames001@gmail.com>
2022-11-07 18:04:15 +0100

    cambio en la configuracion

--- conf.sh
+++ conf.sh
@@ -46,5 +46,19 @@ nvim +PackerSync
 cp config/kitty ~/.config/kitty
 
 #INSTALAMOS ZSH Y CONFIGURAMOS POWERLEVEL10K
-sudo apt install zsh
+sudo apt install zsh -y
 chsh -s $(which zsh)
+cp zshrc ~/.zshrc
+sudo apt install zsh-syntax-highlighting zsh-autosuggestions locate bat -y
+sudo updatedb
+sudo mkdir /usr/share/zsh-sudo
+sudo chown scepticg:scepticg /usr/share/zsh-sudo
+wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/plugins/sudo/sudo.plugin.zsh
+sudo mv sudo.plugin.zsh /usr/share/zsh-sudo
+wget https://github.com/Peltoche/lsd/releases/download/0.23.1/lsd-musl_0.23.1_amd64.deb
+sudo dpkg -i lsd-musl_0.23.1_amd64.deb
+mkdir -p ~/.local/bin
+ln -s /usr/bin/batcat ~/.local/bin/bat
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+/usr/bin/zsh
--- zsh.sh
+++ zsh.sh
@@ -14,3 +14,4 @@ mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+/usr/bin/zsh
