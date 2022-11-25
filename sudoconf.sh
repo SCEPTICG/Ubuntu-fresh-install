3f5cacabe12f46901942ccd3579f1fd07c2c7582
SCEPTICG <scepticgames001@gmail.com>
2022-11-25 22:53:01 +0100

    Update sudoconf.sh

--- sudoconf.sh
+++ sudoconf.sh
@@ -11,6 +11,7 @@ ln -s -f /home/scepticg/.zshrc /root/.zshrc
 chsh -s $(which zsh)
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >> ~/.zshrc
+cp p10k.zsh ~/.p10k.zsh
 
 #INSTALAMOS ASTROVIM PARA ROOT
 git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
